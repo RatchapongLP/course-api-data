67789e33359c7634e6975764d95939770eab5e80
RatchapongLP <ratchapong.lpz@gmail.com>
2025-10-17 17:24:08 +0700

    Add doc comments

--- src/main/java/com/satsumaimo/springbootstarter/course/CourseController.java
+++ src/main/java/com/satsumaimo/springbootstarter/course/CourseController.java
@@ -27,18 +27,43 @@ public class CourseController {
 		return courseService.getCourse(id);
 	} 
 	
+	/**
+	 * Create a course record in the database with id, name, description as sent in the JSON request body.
+	 * The topic associated with the request body {@code course} will be overwritten with an empty one whose
+	 * id is {@code topicId} from the path parameter. The {@code topicId} should point to an existing
+	 * topic record in the database, or else HTTP 500 Internal Server Error will be shown.
+	 * @param course the new course to add to the database
+	 * @param topicId the id of an existing topic in the database to bind the {@code course} with.
+	 * @throws EntityNotFoundException if the {@code topicId} is not found in the database
+	 */
 	@RequestMapping(method=RequestMethod.POST, value="/topics/{topicId}/courses")
 	public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
 		course.setTopic(new Topic(topicId, "", ""));
 		courseService.addCourse(course);
 	}
 	
+	/**
+	 * Update a course record in the database with id, name, description as sent in the JSON request body.
+	 * The topic associated with the request body {@code course} will be overwritten with an empty one whose
+	 * id is {@code topicId} from the path parameter. The {@code topicId} should point to an existing
+	 * topic record in the database, or else HTTP 500 Internal Server Error will be shown.
+	 * @param course the course to update to the database
+	 * @param topicId the id of an existing topic in the database to bind the {@code course} with.
+	 * @throws EntityNotFoundException if the {@code topicId} is not found in the database
+	 */
 	@RequestMapping(method=RequestMethod.PUT, value="/topics/{topicId}/courses/{id}")
 	public void updateCourse(@RequestBody Course course, @PathVariable String topicId, @PathVariable String id) {
 		course.setTopic(new Topic(topicId, "", ""));
 		courseService.updateCourse(course);
 	}
 	
+	/**
+	 * Delete a course record in the database with the path parameter {@code id}.
+	 * If a course with id equal to {@code id} is not found in the database, nothing is done.
+	 * If such course is found, it will be deleted.
+	 * Note: {@code topicId} is not used and need not be correct or existing.
+	 * @param id the id of the course to remove from the database
+	 */
 	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{topicId}/courses/{id}")
 	public void deleteCourse(@PathVariable String id) {
 		courseService.deleteCourse(id);
